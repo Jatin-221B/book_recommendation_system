8ee3cd6af212a3cb7b9f052dc6f8c22446e06c10
Jatin-221B <jatin.bhate29@gmail.com>
2026-02-13 18:07:59 +0530

    Added: Books and Author controller and views

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,10 @@
+class AuthorsController < ApplicationController
+  def index
+    @authors = Author.all.order(:name)
+  end
+
+  def show
+    @author = Author.find(params[:id])
+    @books = @author.books.order(:title)
+  end
+end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.includes(:author)
   end
 
   def show
+    @book = Book.includes(:author, reviews: :user).find(params[:id])
+    @reviews = @book.reviews.order(created_at: :desc)
+    @average_rating = @book.reviews.average(:rating)&.round(1)
   end
 end
