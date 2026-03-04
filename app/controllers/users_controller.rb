4afbb958aadd22d57953c28092467599fb24dd98
Jatin-221B <jatin.bhate29@gmail.com>
2026-03-04 10:36:43 +0530

    Minor authentication changes in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    if !@user.nil?
-      @reviews = @user.reviews.includes(:book).order(created_at: :desc)
-      @favourite_books = @user.favourited_books.includes(:author).order(:title)
+    if current_user.admin? || @user == current_user
+      if !@user.nil?
+        @reviews = @user.reviews.includes(:book).order(created_at: :desc)
+        @favourite_books = @user.favourited_books.includes(:author).order(:title).page(params[:page]).per(9)
+      end
+    else
+      redirect_to root_path, alert: "You are not authorized to view this page."
     end
   end
 end
